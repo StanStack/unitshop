aee90f09219d6f616cc1b67ebbddffcb66a339bf
stanstack <stanstack@163.com>
2017-12-11 17:18:49 +0800

    12.11

---
+++ cart.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/12/11.
+ */
+var vm=new Vue({
+    el:"#app",
+    data:{
+        title:"hello vue"
+    },
+    filters:{
+
+    },
+    mounted:function(){
+
+    },
+    mothods:{
+
+    }
+})
