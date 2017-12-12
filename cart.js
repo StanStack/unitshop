71897110bca37060c67396f7fe59d86dbc9c269c
stanstack <stanstack@163.com>
2017-12-12 11:42:12 +0800

    12.12

--- cart.js
+++ cart.js
@@ -1,18 +1,26 @@
 /**
  * Created by Administrator on 2017/12/11.
  */
-var vm=new Vue({
+var vm = new Vue({
     el:"#app",
     data:{
-        title:"hello vue"
+        totalMoney=0,
+        productList:[]
     },
     filters:{
 
     },
     mounted:function(){
-
+        this.cartView();
     },
-    mothods:{
+    methods:{
+        cartView: function () {
+            var _this=this;
+            this.$http.get("data/cartData.json").then(function(res){
+                _this.productList=res.result.list;
+                _this.totalMoney=res.result.totalMoney;
+            })
+        }
+    },
 
-    }
 })
