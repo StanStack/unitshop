38e403c098d901890908f67bc7ec87053fe8d140
stanstack <stanstack@163.com>
2017-12-15 10:43:16 +0800

    12.12

--- cart.js
+++ cart.js
@@ -8,19 +8,29 @@ var vm = new Vue({
         productList:[]
     },
     filters:{
-
+        formatMoney:function(value){
+            return "￥"+value.toFixed(2);
+        }
     },
     mounted:function(){
-        this.cartView();
+        this.$nextTick(function(){
+            this.cartView();
+        });
     },
     methods:{
         cartView: function () {
             var _this=this;
             this.$http.get("data/cartData.json",{"id": 123}).then(function(res){
-                _this.productList=res.data.result.list;
+                vm.productList=res.data.result.list;
                 _this.totalMoney=res.data.result.totalMoney;
             })
         }
     },
 
+})
+
+
+//全局过滤器
+Vue.filter("money",function(value,type){
+    return "¥" + value.toFixed(2)+type;
 })
